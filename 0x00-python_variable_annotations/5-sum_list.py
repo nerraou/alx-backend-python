22e65108ae71e536b64e9c9d47f0e298f8a0783b
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-16 00:25:52 +0100

    sum float list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+sum of float list function
+"""
+from typing import List
+
+
+def sum_list(floatList: List[float]) -> float:
+    """
+    sum of float list
+    """
+    return sum(floatList)
