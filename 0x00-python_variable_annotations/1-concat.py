17c01143e0805551e2c315f60ad33ab10ce2f8ef
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-15 23:39:57 +0100

    concat strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type concat function to add two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat two strings
+    """
+    return str1 + str2
