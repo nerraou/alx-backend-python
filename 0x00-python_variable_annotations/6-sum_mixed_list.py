faf7ff32b5d7175bb674bc5a22ff6bbbfdf56e0b
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-02-16 00:40:26 +0100

    fix variable name

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ sum of float list function
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     sum of float list
     """
