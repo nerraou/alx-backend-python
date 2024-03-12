607e9674f67ea279e4b7a41e44a76611f2b29d4a
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-12 00:21:15 +0000

    feat: test_public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,7 @@ Unittest for test_access_nested([..])
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -24,6 +24,19 @@ class TestGithubOrgClient(unittest.TestCase):
 
         url = f"https://api.github.com/orgs/{org_name}"
         mock.assert_called_once_with(url)
+
+    def test_public_repos_url(self):
+        """test public repo url"""
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mock:
+            response = {"repos_url": "mock_repo_url"}
+
+            mock.return_value = response
+
+            test_class = GithubOrgClient("test_repo_name")
+
+            result = test_class._public_repos_url
+            self.assertEqual(result, response["repos_url"])
 
 
 if __name__ == "__main__":
