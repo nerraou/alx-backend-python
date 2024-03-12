c905aa5780d04f0fde29e80495c375d8c342c8b0
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-12 00:36:19 +0000

    feat: test_public_repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -57,6 +57,15 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public.assert_called_once()
             mock.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """test has_license"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
